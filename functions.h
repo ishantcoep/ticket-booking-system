2f98c2533b07f05b50c08bde6829b70b621cf56f
ishantcoep <ishant22696@gmail.com>
2015-10-26 23:40:16 +0530

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void first_time();
+void repeated_user();
+void passwordcheck(int c);
+void ticketdisplay();
+void ticketbookingandpayment();
+void seatbooking();
+void ticketprint(int a, int b);
+void ticketarray();
+void show_display();
