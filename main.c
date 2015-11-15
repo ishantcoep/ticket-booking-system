2a5788fdc140901f0768ff9c7813f04af27a4393
ishantcoep <ishant22696@gmail.com>
2015-11-16 00:44:07 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
+/*****************************************************************************
+ * Copyright (C) ISHANT PAKMODE ishant22696@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *****************************************************************************/
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include"functions.h"
 
 int main() {
-	int w, i;
-	char *admin = "ishant";
+	int w;
 	char *admin_pass = "123456789";
-	char *a, *b;
+	char a[8], b[10];
 	
 	
-	FILE *files[10];
-	for (i = 0; i < 10; i++) {
-		char filename[15];
-		sprintf(filename, "show%d.txt", i);
-		files[i] = fopen(filename, "w");
-		fclose(files[i]);
-	}
+	
 	printf("\t\t\tWelcome to Movie Ticket Booking System:\n");
 	printf("\t\t\tNew to this system ??? Sign up for free !!!\n");
 	printf("Choose :\n\t\t\t 1. Register/Sign up\n\t\t\t2. User Login\n\t\t\t3. Admin login\n");
@@ -32,16 +40,13 @@ int main() {
 		case 3 :
 			printf("Enter admin username :");
 			scanf("%s", a);
-			if(strcmp(a, admin) == 0){
-				printf("\nNow enter admin password :");
-				scanf("%s", b);
-				if(strcmp(a, admin_pass) == 0)
-					printf("Welcome Mr. Admin !!");
-					//admin_page(); //incomplete*******
+			if(strcmp(a, "admin") == 0) {
+				admin_page(); 
 			}
-			else
+			else {
 				printf("You're not the admin !! Please Login or Register !!");
 				main();
+			}
 			break;
 		default :
 			printf("Please enter a valid choice!!");
